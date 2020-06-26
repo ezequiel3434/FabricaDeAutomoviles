fc44e226755a47d4bce7cd031c641f017f6f9657
Ezequiel <ezequiel3434@gmail.com>
2020-06-26 19:30:46 -0300

    Initial Commit

---
+++ FabricaDeAutomoviles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FabricaDeAutomoviles
+//
+//  Created by Ezequiel Parada Beltran on 26/06/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
